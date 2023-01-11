f013ffdff6f4228384c7aad13143a2978724e739
Antonio Esposito <antonio.esposito4@live.it>
2023-01-11 21:48:33 +0100

    Create new project for Action filters

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,8 +4,9 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Versioning;
 
-namespace WebAPI.Controllers
+namespace Versioning.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace WebAPI
+namespace Versioning
 {
     public class Program
     {
